f97fa8c3743f3a968a100decea49141ee025f24b
anupritaisno1 <www.anuprita804@gmail.com>
2021-01-24 01:09:48 +0530

    fix 5967f17e179834fa05c1506a3721c85caa74aab9 squash this onto 5967f17e179834fa05c1506a3721c85caa74aab9
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -126,13 +126,13 @@ static int sixty __read_only  = 60;
 static int __maybe_unused neg_one __read_only = -1;
 static int __maybe_unused neg_three __read_only = -3;
 
-static int zero;
+static int zero __read_only;
 static int __maybe_unused one __read_only = 1;
 static int __maybe_unused two __read_only = 2;
 static int __maybe_unused three __read_only = 3;
 static int __maybe_unused four __read_only = 4;
-static unsigned long zero_ul;
-static unsigned long one_ul = 1;
+static unsigned long zero_ul __read_only;
+static unsigned long one_ul __read_only = 1;
 static unsigned long long_max __read_only = LONG_MAX;
 static int one_hundred __read_only = 100;
 static int one_thousand __read_only = 1000;
