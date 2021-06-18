16734af51a105154bb900b4b9d09699b49b9dd9c
Pratyush <codelab@pratyush.dev>
2021-06-18 14:51:34 +0530

    ipv6 privacy address fix
    skip check before creating new ipv6 privacy address
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -2285,7 +2285,6 @@ regen:
 
 static void  ipv6_try_regen_rndid(struct inet6_dev *idev, struct in6_addr *tmpaddr)
 {
-	if (tmpaddr && memcmp(idev->rndid, &tmpaddr->s6_addr[8], 8) == 0)
 		ipv6_regen_rndid(idev);
 }
 
